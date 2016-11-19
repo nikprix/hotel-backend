4a43af45e94b323ffeee3f2d9bde09b01379a839
nikprix <gitkola@gmail.com>
2016-11-19 18:28:22 -0500

    Added SQL flies

---
+++ src/test/java/com/mykolabs/test/DBUnitTest.java
@@ -0,0 +1,21 @@
+package com.mykolabs.test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author nikprixmar
+ */
+public class DBUnitTest {
+    
+    public DBUnitTest() {
+    }
+    
+}
